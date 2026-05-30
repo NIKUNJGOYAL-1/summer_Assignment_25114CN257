3e8cc8cc34e59dbbaf96dc3b2dade7bf19f55670
Nikunj Goyal <25114cn257@glbitm.ac.in>
2026-05-30 16:35:27 +0530

    first commit

--- Day1/code1.java
+++ Day1/code1.java
@@ -1,7 +1,7 @@
-package Day1;
+p ackage Day1;
 
 import java.util.Scanner;
-public class code1 {
+public class code1{
 public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
     int n=sc.nextInt();
